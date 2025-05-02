d590c3cd0f2509c15d9c5a1c786dd06ee2291a34
Woliks <stian.skryten@gmail.com>
2025-05-02 12:59:54 +0200

    Added more of the exame

--- Eksamen 24 Oving/Eksamen 24 Oving.cpp
+++ Eksamen 24 Oving/Eksamen 24 Oving.cpp
@@ -1,7 +1,74 @@
 import std;
 
+class Person {
+private:
+	std::string navn;
+	std::string adresse;
+	std::string tlf;
+public:
+	Person(std::string navn, std::string adresse, std::string tlf);
+};
+
+class Ansatt :
+	public Person
+{
+private:
+	std::string stilling;
+	bool deltid;
+public:
+	Ansatt(std::string navn);
+};
+
+class Student :
+	public Person
+{
+private:
+	std::string studentId;
+	std::deque<std::unique_ptr<Emne>> emneListe;
+public:
+	Student() = default;
+	Student(std::string navn, std::string adr, std::string tlf, std::string studentID);
+};
+
+class Institutt {
+private:
+	std::string navn;
+	std::string adresse;
+	std::deque<std::unique_ptr<Ansatt>> ansattListe;
+	std::deque<std::unique_ptr<Emne>> emneListe;
+	std::deque<std::unique_ptr<Student>> studentListe;
+public:
+	Institutt(std::string navn, std::string adresse);
+	bool leggTilEmne(std::string emneKode, std::string emneNavn, std::string ansattnavn)
+	{
+		emneListe.push_back(std::unique_ptr<Emne>(new Emne(emneKode, emneNavn, Ansatt(ansattnavn))));
+	}
+	void skrivUtEmner();
+	void registrerEmneForStudent(std::string emneKode, std::string studentnavn);
+};
+
+class Emne {
+private:
+	std::string emnekode;
+	std::string emnenavn;
+	std::string foreleser;
+	std::deque<std::unique_ptr<Emne>> emneListe;
+	std::deque<std::unique_ptr<Student>> studentListe;
+public:
+	Emne(std::string emnekode, std::string emnenavn, Ansatt foreleser);
+};
+
+class Karakter {
+private:
+	Student student;
+	Emne emne;
+	std::string karakter;
+public:
+	Karakter(Student student, Emne emne, std::string karakter);
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 }
 
